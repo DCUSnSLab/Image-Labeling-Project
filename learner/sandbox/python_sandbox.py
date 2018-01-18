3044b0d4f8fae065229a8c213f035b10698cef65
KwangEun An <yooer10@cu.ac.kr>
2018-01-18 13:45:42 +0900

    파이선 언어 연습 코드 추가

---
+++ learner/sandbox/python_sandbox.py
@@ -0,0 +1,20 @@
+"""
+파이선 언어 샌드박스 코드
+저자 : 안광은 (yooer10ms@cu.ac.kr)
+버전 : python 3.5.4
+
+자꾸 파이선 언어 잊어머거서 결국 테스트 코드 용으로 만들어둬야겟...
+레퍼런스:
+https://pythonspot.com/pyqt5/
+
+"""
+
+# 문자열(string) 합치기(concatenation) 테스트
+
+# + 사용해서 합쳐보기
+str_1 = 'testing'
+str_2 = 'a concatenation'
+print(str_1 + str_2) # 진짜 잘됨 ㅇㅅㅇ
+
+# 중간에 고정 문자 끼워보기
+print(str_1 + ' ' + str_2)
