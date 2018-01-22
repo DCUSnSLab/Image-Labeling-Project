0574083298d6635c94dbc99ad1d87426b207bb1c
JeongHan <gkswjd9969@gmail.com>
2018-01-22 14:54:54 +0900

    web test

--- webEngine/index.php
+++ webEngine/index.php
@@ -28,7 +28,7 @@
     </head>
     <body class="main">
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a class="navbar-brand" href="/?theme=2">
+            <a class="navbar-brand" href="/var/www/html/Image-Labeling-Project/webEngine">
                 "Web for Image Classification EX"
             </a>
         </nav>
