a4cd6a5da657408af6fc3773d534412cdb27e556
JeongHan <gkswjd9969@gmail.com>
2018-01-22 14:50:25 +0900

    web test

--- webEngine/index.php
+++ webEngine/index.php
@@ -14,12 +14,33 @@
 <html lang="kr">
     <head>
         <meta charset-"utf-8">
+        <style>
+            .display-5{
+                font-size:3rem;
+                font-weight:300;
+                line-height:1.2
+            }
+        </style>
     </head>
-    <body class="page-bootswatch">
+    <body class="main">
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <a class="navbar-brand" href="/?theme=2">
                 "Web for Image Classification EX"
             </a>
         </nav>
+        <div class="jumbotron text-center">
+            <h1 class="display-5"> title </h1>
+            <p class="lead">
+                "subtitle"
+            </p>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-9">
+                </div>
+                <div class="col-12 col-md-3">
+                </div>
+            </div>
+        </div>
     </body>
 </html>
