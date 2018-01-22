06f15b045c4e6970d950b1dd790c5019763993fd
JeongHan <gkswjd9969@gmail.com>
2018-01-22 13:52:02 +0900

    web test

---
+++ webEngine/index.php
@@ -0,0 +1,26 @@
+<!--
+    @author
+        Jeong Han
+    @library
+        bootstrap CDN
+    @date
+        2018.01.22
+    @title
+        Web for Image Classification
+-->
+<!-- Combined and minimized CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+
+<!-- Contrib theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0-beta.3/cyborg/bootstrap.min.css">
+
+<html lang="kr">
+    <head>
+        <!-- Combined and minimized JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    </head>
+    <body class="page-bootswatch">
+    </body>
+</html>
