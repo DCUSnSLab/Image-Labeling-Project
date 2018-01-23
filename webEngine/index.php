c09f1695b7c1f694a3da28ed4f99300780f43511
JeongHan <gkswjd9969@gmail.com>
2018-01-23 18:00:31 +0900

    카운트 다운 추가
    테스팅

--- webEngine/index.php
+++ webEngine/index.php
@@ -10,23 +10,12 @@
 -->
 <!-- bootstrap theme "Cyborg"-->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0-beta.3/cyborg/bootstrap.min.css">
-
-<html lang="kr">
+<link rel="stylesheet" href="default.css" type="text/css" />
+<html>
     <head>
         <meta charset-"utf-8">
-        <style>
-            .display-5{
-                font-size:3rem;
-                font-weight:300;
-                line-height:1.2
-            }
-            .lead{
-                font-size:2rem;
-                font-weight:200
-            }
-        </style>
     </head>
-    <body class="main">
+    <body>
         <!-- Menu Bar -->
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <a class="navbar-brand" href="./index.php">
@@ -35,13 +24,23 @@
         </nav>
         <!-- Title Box -->
         <div class="jumbotron text-center">
-            <h1 class="display-5"> title </h1>
-            <p class="lead">
+            <h1 class="title"> title </h1>
+            <p class="subtitle">
                 "subtitle"
             </p>
         </div>
         <!-- Main Box -->
         <div class="container">
+            <p><span id="countdowntimer"> 60 </span> Seconds later</p>
+            <script type="text/javascript">
+                var timeleft = 60;
+                var downloadTimer = setInterval(function(){
+                    timeleft--;
+                    document.getElementById("countdowntimer").textContent = timeleft;
+                    if(timeleft <= 0)
+                        clearInterval(downloadTimer);
+                },1000);
+            </script>
             <!-- Center -->
             <div class="row">
                 <!-- Image Box -->
@@ -52,5 +51,14 @@
                 </div>
             </div>
         </div>
+        <!-- tail text -->
+        <footer class="mt-5 py-4 text-center">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                    </div>
+                </div>
+            </div>
+        </footer>
     </body>
 </html>
